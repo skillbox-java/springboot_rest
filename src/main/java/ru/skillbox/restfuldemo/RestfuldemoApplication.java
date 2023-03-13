b142b1ffb15e2a7bdb7b769e41d70132a068765d
Konstantin Shibkov <shibkov.k@gmail.com>
2023-03-13 11:16:41 +0500

    init

---
+++ src/main/java/ru/skillbox/restfuldemo/RestfuldemoApplication.java
@@ -0,0 +1,13 @@
+package ru.skillbox.restfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfuldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfuldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillbox/restfuldemo/RestfuldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillbox.restfuldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfuldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
